8847b7a0a9352bd15fe4f4d7be08aa13768ac736
Hugo Holmqvist <hugo.holm@live.com>
2019-03-10 01:03:03 +0200

    Begin site rework

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Head from './components/Head'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Head />
+        <div className="shitstorm">
+          <img src="/img/shitstorm.svg" width="200 200"/>
+        </div>
+        <div className="lines">
+          <img src="/img/lines.svg" width="400 400" />
+        </div>
       </div>
     );
   }
--- src/components/Head.js
+++ src/components/Head.js
@@ -7,12 +7,10 @@ export default function Head(props) {
   const {title, subtitle, date} = headContent
 
   return (
-    <div className="head-container">
-      <div className="hero-content">
-        <h1 className="hero-title">{title}</h1>
-        <h2 className="hero-subtitle">{subtitle}</h2>
-        <h3 className="hero-subtitle">{date}</h3>
-      </div>
+    <div className="hero-content">
+      <h1 className="hero-title">{title}</h1>
+      <h2 className="hero-subtitle">{subtitle}</h2>
+      <h3 className="hero-subtitle">{date}</h3>
     </div>
   )
 }
