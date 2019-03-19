a024fb017cdd8b84857cc691effb27a160c5b2bc
Hugo Holmqvist <hugo.holm@live.com>
2019-03-19 14:25:22 +0200

    Funny hovering clouds

--- src/components/Head.js
+++ src/components/Head.js
@@ -4,15 +4,21 @@ import './styles/head.css'
 
 
 export default function Head(props) {
-  const {title, subtitle, date} = headContent
+  const {title, subtitle, date} = headContent 
 
   return (
     <div className="hero-content">
       <div className="hero-content-wrapper">
-        <img className="head-image" src="/img/bleh.jpeg"></img>
         <h1 className="hero-title">{title}</h1>
         <h2 className="hero-subtitle">{subtitle}</h2>
         <h3 className="hero-subtitle">{date}</h3>
+        <img src="/img/isovitunkirkko.png" className="head-isovitunkirkko" />
+        <img src="/img/cloud.png" className="head-cloud cloud-translate" />
+        <img src="/img/cloud2.png" className="head-cloud c2 cloud-translate" />
+        <img src="/img/cloud.png" className="head-cloud c3 cloud-translate" />
+        <img src="/img/cloud.png" className="head-cloud c4 cloud-translate" />
+        <img src="/img/cloud2.png" className="head-cloud c5 cloud-translate" />
+        <img src="/img/cloud2.png" className="head-cloud c6 cloud-translate" />
       </div>
     </div>
   )
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -3,12 +3,11 @@ import infoContent from '../content/info.json'
 import './styles/content.css'
 
 export default function Schedule() {
-  const {title, content} = infoContent
   return (
     <div className="content">
       <div className="content-content-box inverted">
         <div className="content-text-wrapper">
-          <p className="content-text">{content}</p>
+          <p className="content-text">TBA</p>
         </div>
       </div>
 
