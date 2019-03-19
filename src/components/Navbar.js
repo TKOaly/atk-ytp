6bf2fa3d6ec70cb2bd3521a44699a0f5515de667
alehuo <aleksi.huotala@helsinki.fi>
2019-03-19 16:12:45 +0200

    Rename navbar.scss to navbar.module.scss and modify Navbar.js to enable the use of CSS modules

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import './styles/navbar.scss'
+import styles from './styles/navbar.module.scss'
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <ul className="navbar__items">
-        <li className="navbar__item">etusivu</li>
-        <li className="navbar__item">yleisinfo</li>
+    <div className={styles.navbar}>
+      <ul className={styles["navbar__items"]}>
+        <li className={styles["navbar__item"]}>etusivu</li>
+        <li className={styles["navbar__item"]}>yleisinfo</li>
       </ul>
     </div>
   )
