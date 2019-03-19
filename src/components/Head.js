9d63ee5eec1834f7123e00fb6d3f9a207776601a
Hugo Holmqvist <hugo.holm@live.com>
2019-03-19 15:25:01 +0200

    Snapshots of site for SEO

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,20 +6,24 @@ import './styles/head.css'
 export default function Head(props) {
   const {title, subtitle, date} = headContent 
 
+  const createClouds = () => {
+    const clouds = ['cloud.png', 'cloud2.png']
+    const arr =  Array
+      .apply(null, {length: 6})
+      .map((_, i) => (<img key={i} src={`/img/${clouds[Math.floor(Math.random() * 2)]}`} alt="" className={`head-cloud ${i > 0 ? 'c' + (i + 1) : ''} cloud-translate`} />))
+    console.log(arr)
+    return arr
+  }
+
   return (
     <div className="hero-content">
       <div className="hero-content-wrapper">
         <h1 className="hero-title">{title}</h1>
         <h2 className="hero-subtitle">{subtitle}</h2>
         <h3 className="hero-subtitle">{date}</h3>
-        <img src="/img/isovitunkirkko.png" className="head-isovitunkirkko" />
-        <img src="/img/cloud.png" className="head-cloud cloud-translate" />
-        <img src="/img/cloud2.png" className="head-cloud c2 cloud-translate" />
-        <img src="/img/cloud.png" className="head-cloud c3 cloud-translate" />
-        <img src="/img/cloud.png" className="head-cloud c4 cloud-translate" />
-        <img src="/img/cloud2.png" className="head-cloud c5 cloud-translate" />
-        <img src="/img/cloud2.png" className="head-cloud c6 cloud-translate" />
+        <img src="/img/isovitunkirkko.png" alt="" className="head-isovitunkirkko" />
+        {createClouds()}
       </div>
     </div>
-  )
+  ) 
 }
