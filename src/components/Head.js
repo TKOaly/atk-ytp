69d1e0fe0075a8ab8a7680d8f6633bdfea7c90a3
Hugo Holmqvist <hugo.holm@live.com>
2019-03-22 14:54:03 +0200

    Countdown timer

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,30 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import headContent from '../content/hero.json'
 import './styles/head.scss'
 
-export default function Head (props) {
+export default function Head () {
   const { title, subtitle, date } = headContent
-
-  const createClouds = () => {
-    const clouds = ['cloud.png', 'cloud2.png']
-    const arr = Array.apply(null, { length: 8 }).map((_, i) => (
-      <img
-        key={i}
-        src={`/img/${clouds[Math.floor(Math.random() * 2)]}`}
-        alt=""
-        className={`head-cloud c${i+1} cloud-translate`}
-      />
-    ))
-    console.log(arr)
-    return arr
-  }
 
   return (
     <div className="hero-content">
       <div className="hero-content-wrapper">
         <h1 className="hero-title">{title}</h1>
         <h2 className="hero-subtitle">{subtitle}</h2>
-        <h3 className="hero-subtitle">{date}</h3>
+        <CountdownTimer startDate={date} />
         <img
           src="/img/isovitunkirkko.png"
           alt=""
@@ -33,5 +19,42 @@ export default function Head (props) {
         {createClouds()}
       </div>
     </div>
+  )
+}
+
+function createClouds () {
+  const clouds = ['cloud.png', 'cloud2.png']
+  const arr = Array.apply(null, { length: 6 }).map((_, i) => (
+    <img
+      key={i}
+      src={`/img/${clouds[Math.floor(Math.random() * 2)]}`}
+      alt=""
+      className={`head-cloud ${i > 0 ? 'c' + (i + 1) : ''} cloud-translate`}
+    />
+  ))
+  console.log(arr)
+  return arr
+}
+
+function CountdownTimer ({ startDate }) {
+  const [timeLeft, tick] = useState(new Date())
+  useEffect(() => {
+    const intervalId = setInterval(() => tick(new Date()), 1000)
+    return () => clearInterval(intervalId)
+  })
+  console.log(timeLeft)
+
+  const parseTimeValues = (date) => {
+    const distance = new Date(startDate).getTime() - date.getTime()
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+    return distance > 0 ? `${days} päivää, ${hours} tuntia, ${minutes} minuuttia, ${seconds} sekunttia` : 'OHI ON!'
+  }
+
+  return (
+    <h3 className="hero-subtitle countdown">{parseTimeValues(timeLeft)}</h3>
   )
 }
