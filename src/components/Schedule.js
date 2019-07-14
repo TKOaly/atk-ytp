dc5b4761387493a8ca9b37188666da8dfcdcd7a2
Hugo Holmqvist <hugo.holm@live.com>
2019-07-14 20:57:46 +0300

    Add new cool icons

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -8,8 +8,30 @@ export default function Schedule () {
         <h1 className="title-text timetable">Aikataulu</h1>
       </div>
       <div className="content-content-box timetable">
+        <div className="info-content-background-box timetable">
+          <div className="schedule-icon wed" />
+        </div>
         <div className="content-text-wrapper">
-          <p className="content-text">TBA</p>
+          <h2 className="schedule-day__header">Keskiviikko</h2>
+          <p className="content-text">Aikataulut tulevat myöhemmin näkyviin!</p>
+        </div>
+      </div>
+      <div className="content-content-box timetable">
+        <div className="info-content-background-box timetable">
+          <div className="schedule-icon thu" />
+        </div>
+        <div className="content-text-wrapper">
+          <h2 className="schedule-day__header">Torstai</h2>
+          <p className="content-text">Aikataulut tulevat myöhemmin näkyviin!</p>
+        </div>
+      </div>
+      <div className="content-content-box timetable">
+        <div className="info-content-background-box timetable">
+          <div className="schedule-icon fri" />
+        </div>
+        <div className="content-text-wrapper">
+          <h2 className="schedule-day__header">Perjantai</h2>
+          <p className="content-text">Aikataulut tulevat myöhemmin näkyviin!</p>
         </div>
       </div>
     </div>
