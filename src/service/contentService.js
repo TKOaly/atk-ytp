a363c183f72116a876add99181e6b920bfff8358
Hugo Holmqvist <hugo.holm@live.com>
2019-08-07 23:45:36 +0300

    CMS implementation

---
+++ src/service/contentService.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+const PROD_URL = 'https://mysterious-wave-58026.herokuapp.com/api/content/calendars/ke'
+
+const resolveUri = () =>
+  typeof window === 'undefined' ? PROD_URL : (window.location.hostname === 'localhost' ? 'http://localhost:3000' : PROD_URL)
+
+export const getInfo = () =>
+  axios
+    .get(`${resolveUri()}/api/content/info`)
+    .then(({ data }) => ({
+      title: data.info.title,
+      text: data.info.content
+    }))
+
+export const getSchedule = day =>
+    axios
+      .get(`${resolveUri()}/api/content/calendars/${day}`)
+      .then(({data}) => data)
